bde8ace484d5213c38e5b1fe030956ce74225c37
Ryan Cooke <ryan@balena.io>
2026-02-24 10:12:38 +0000

    Add retry to serial port opening
    When using serial to detect provisioning is completed, there may be a delay before the serial port enumerates, in the case of some devices. This retry stops an error if this happens
    
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- lib/flashing/powerDetection/serial.ts
+++ lib/flashing/powerDetection/serial.ts
@@ -1,6 +1,6 @@
 import { Autokit } from '../../';
 import { ReadlineParser } from '@serialport/parser-readline';
-import { Readable } from 'serialport';
+import * as retry from 'bluebird-retry';
 
 const powerOffMessage = process.env.POWER_OFF_MESSAGE || 'reboot: Power down'
 const timeout = Number(process.env.POWER_OFF_TIMEOUT) || 1000*60*5;
@@ -9,8 +9,14 @@ const timeout = Number(process.env.POWER_OFF_TIMEOUT) || 1000*60*5;
  * Checks whether the DUT has powered down by checking for a serial message
  **/
 export async function waitForPowerOffSerial(autoKit:Autokit):Promise<void> {
-    const serialport = await autoKit.serial.open();
-
+    let serialport: any;
+    await retry(async () => {
+        serialport = await autoKit.serial.open();
+    },
+    {
+        interval: 500,
+        max_tries: 10       
+    })
     if(serialport !== undefined){
         return new Promise<void>((resolve, reject) => {
 
